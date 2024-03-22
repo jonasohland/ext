694f263730eca0a72e7f01fc21980d3b3444116a
Jonas Ohland <jonas.ohland@gmail.com>
2024-03-22 21:39:10 +0100

    swap-like semantics for LastError

--- errors/last.go
+++ errors/last.go
@@ -12,13 +12,25 @@ type lastErrorWrapper struct {
 }
 
 // Set the error atomically, future calls to Get() will return this error
-func (l *LastError) Set(err error) {
-	l.v.Store(lastErrorWrapper{err})
+func (l *LastError) Set(err error) error {
+	v := l.v.Swap(lastErrorWrapper{err})
+	wrapper, ok := v.(lastErrorWrapper)
+	if !ok {
+		return nil
+	}
+
+	return wrapper.v
 }
 
 // Clear the error, future calls to Get() will return nil
-func (l *LastError) Clear() {
-	l.v.Store(lastErrorWrapper{})
+func (l *LastError) Clear() error {
+	v := l.v.Swap(lastErrorWrapper{})
+	wrapper, ok := v.(lastErrorWrapper)
+	if !ok {
+		return nil
+	}
+
+	return wrapper.v
 }
 
 // Get the error that was last stored with Store(), or nil if Clear() was called
